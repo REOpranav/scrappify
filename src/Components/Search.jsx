262e3ff341aed8a9a771b54e75afea468ae287de
pranavavadivel <pranavavadivel01@example.com>
2025-05-06 02:06:44 +0530

    Addded new components and stored the potencial header only in D

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import '../Css/Search.css';
-import React, { useActionState, useContext, useEffect, useInsertionEffect, useState } from 'react'
+import React, { useActionState, useContext, useEffect, useInsertionEffect, useRef, useState } from 'react'
 import { Flex, Row, Col, Typography, Input, Button, Divider } from 'antd'
 import { SearchOutlined } from "@ant-design/icons";
 
@@ -9,25 +9,48 @@ const handleSubmit = (params, formvalue) => {
 }
 
 const Search = () => {
-    const [formValue, submit, pending] = useActionState(handleSubmit, "");
-    const [finalHTML, setFinalHtml] = useState([])
 
-    useEffect(() => {
+    const [formValue, submit, pending] = useActionState(handleSubmit, "");
+    const [totalHeadDetail, setTotalHeadDetail] = useState([])
+    const [totalDetail, setTotalDetail] = useState([])
+    const [dbStoringHeads, setDbStoringDatas] = useState([])
+
+    // this detail for storing potencial data only in DB
+    const runningTimer = useRef(0) // running time
+    const timerResult = useRef(0) // Final output ( how much time user stay for url )
+
+    useEffect(() => { // get scrapped Data
         if (pending || !formValue) return;
         try {
             let URL = `http://localhost:3002/scrap/search`;
-            axios.post(URL, { formData: formValue }).then(val => setFinalHtml(val.data));
+            timerResult.current = ((Date.now() - runningTimer.current) / 1000).toFixed(2)
+            axios.post(URL, { formData: formValue }).then((val) => {
+                runningTimer.current = Date.now()
+                setTotalHeadDetail(val?.data?.totalHeadDetail) // getting total Head Detail from DB
+                setTotalDetail(val?.data?.totalDetail) // getting all detail when scrapped
+                setDbStoringDatas(val?.data?.dbStoringHeadsAndURL)
+            });
         } catch (err) {
             console.log(err);
         }
     }, [formValue])
 
-    console.log(finalHTML);
+
+    useEffect(() => { // Storing in DB process
+        try {
+            let URL = `http://localhost:3002/db/heads`;
+            axios.post(URL, { timer: timerResult?.current, dbStoringHeadsAndURL: dbStoringHeads }).then((val) => {
+                console.log(val.data);
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    }, [formValue])
 
     return (
         <div className='SearchModule'>
-            <Row>
-                <Col span={24}>
+            <Row justify={'space-around'}>
+                <Col span={12} className='scrappedValueWithHeading'>
                     <Row className='inputSearchField'>
                         <Col span={24}>
                             <form action={submit}>
@@ -39,15 +62,15 @@ const Search = () => {
 
                     <Row className='searchResult'>
                         <Col span={24}>
-                            {!Array.isArray(finalHTML) &&
+                            {!Array.isArray(totalHeadDetail) &&
                                 <main>
                                     <Row style={{ padding: "10px 0" }}>
-                                        <Typography.Link href={finalHTML?.name} target="_blank" copyable>{finalHTML?.name}</Typography.Link>
+                                        <Typography.Link href={totalHeadDetail?.name} target="_blank" copyable>{totalHeadDetail?.URLName}</Typography.Link>
                                     </Row>
 
                                     <Row>
                                         <Col>
-                                            {finalHTML?.data?.map((val) => {
+                                            {totalHeadDetail?.data?.map((val) => {
                                                 return val?.paragraphs?.length > 0 && <Row>
                                                     <Col span={24}>
                                                         <Row>
@@ -64,6 +87,20 @@ const Search = () => {
                                     </Row>
                                 </main>
                             }
+                        </Col>
+                    </Row>
+                </Col>
+                <Col span={10} className='scrappedValueWithParas'>
+                    <Row className='paraResult'>
+                        <Col span={24}>
+                            {totalDetail?.paras?.map((val) => {
+                                return val !== '' && <Row>
+                                    <Col span={24}>
+                                        <Row>{val}</Row>
+                                        <Divider style={{ borderColor: '#4ca30d' }} />
+                                    </Col>
+                                </Row>
+                            })}
                         </Col>
                     </Row>
                 </Col>
